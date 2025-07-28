8a8fdd017543b1fe33e21d2cb465362b4c5cf2f4
Pear_Shop <phuroblox2002@gmail.com>
2025-07-28 13:14:52 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
-# server.py
-from flask import Flask
-from fastapi.responses import FileResponse
+from flask import Flask, send_file
 import os
 
 app = Flask(__name__)
 
 current_command = ""
 
-@app.get("/raw/pear.py")
+@app.route("/raw/pear.py")
 def serve_pear_code():
     path = os.path.join(os.path.dirname(__file__), "pear.py")
-    return FileResponse(path, media_type="text/plain")
+    return send_file(path, mimetype="text/plain")
 
 @app.route("/")
 def home():
@@ -31,6 +29,6 @@ def clear_command():
     global current_command
     current_command = ""
     return "🧹 Đã xóa lệnh"
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
